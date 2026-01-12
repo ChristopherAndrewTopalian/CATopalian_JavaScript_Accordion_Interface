d5a1e92e162b4eaf0413d42678280aa0ef1844ba
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-01-11 19:40:55 -0500

    update

--- src/js/CATopalian_Combine_JS_Files.bat
+++ src/js/CATopalian_Combine_JS_Files.bat
@@ -9,15 +9,21 @@ type nul > "%output%"
 
 :: loop through all JavaScript files in subdirectories
 for /r %%i in (*.js) do (
-    :: append content of each file to output file
+    :: append the content of each file to the output file
     type "%%i" >> "%output%"
 )
 
 echo "JavaScript files combined into %output% successfully."
 
+::::
+
+:: This .bat File Combines All .js files in all folders of our js folder, into one main.js file.
+:: To activate this .bat file, we double click the .bat file, while it is located in our js folder.
+
+::::
+
 :: Dedicated to God the Father
-:: All Rights Reserved Christopher Andrew Topalian Copyright 2000-2025
+:: All Rights Reserved Christopher Andrew Topalian Copyright 2000-2026
 :: https://github.com/ChristopherTopalian
 :: https://github.com/ChristopherAndrewTopalian
-:: This .bat File Combines All .js files in all folders of our project folder, into one main.js file.
-:: To activate this .bat file, we double click the .bat file, while it is located in our js folder.
+
